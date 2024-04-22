ca3aab07b6e542f0d9a625d2ec677945e48efb14
nick <nicholas.diblasio@gmail.com>
2024-04-21 21:06:46 -0700

    Initial Commit

---
+++ vinyl_lastfm/vinyl_lastfmApp.swift
@@ -0,0 +1,17 @@
+//
+//  vinyl_lastfmApp.swift
+//  vinyl_lastfm
+//
+//  Created by Nick DiBlasio on 4/21/24.
+//
+
+import SwiftUI
+
+@main
+struct vinyl_lastfmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
